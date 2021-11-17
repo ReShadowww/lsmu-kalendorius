4e18778372d92e49200d96a5cf1de54f235be37c
ReShadowww <justas.adomas.l@gmail.com>
2021-11-17 15:14:01 +0200

    up

--- kalendorius-lsmu.py
+++ kalendorius-lsmu.py
@@ -1,18 +1,111 @@
-import icalendar
-from time import sleep
-import requests
-import re
-import sys
-import csv
 from datetime import datetime
+from time import sleep
+import icalendar
+import requests
+import csv
+import sys
+import re
 
-CIKLAS = "Krūtinės ligos, alergologija ir klinikinė imunologija"
+from simple_term_menu import TerminalMenu
+
 CAL_URL = "https://lsmusis.lsmuni.lt/Tvarkarasciai/ManoTvarkarastis/ICal?lang=Lt&id=LkRwDqvySWE%3D"
-CSV_FNAME = "tvarkarastis.csv" # Must end with .csv
+CSV_FNAME = "tvarkarastis.csv"  # Must end with .csv
+CAL_FNAME = "cal.ics"
 
 
-def get_shit_list(flocation):
-    fuck_lsmu_list = []
+class Event:
+
+    def __init__(self, description, summary, dtstart, dtend):
+
+        self.description = description
+        self.summary = summary
+        self.dtstart = dtstart
+        self.dtend = dtend
+
+        self.set_numeris()
+        self.set_data()
+        self.set_pavadinimas()
+        self.set_tipas()
+        self.set_ciklas()
+
+    def get_combo_dict(self, ciklas):
+        if self.ciklas == ciklas:
+            combo = {
+                "numeris": self.numeris,
+                "data": self.data,
+                "pavadinimas": self.pavadinimas,
+                "tipas": self.tipas,
+                "ciklas": self.ciklas,
+            }
+            return combo
+
+    def set_numeris(self):
+        pattern = r"([0-9]\.[0-9]+).*?\n"
+        string_to_search = self.description
+        result = re.search(pattern, string_to_search)
+        if result:
+
+            line = result.group(0)
+            number = line.split()[0].strip(".")
+
+            # Kad ištaisyti 4.5 --> 4.05
+            # Kad ištaisyti 4.3/1 --> 4.03/2
+            number_split = number.split(".")
+
+            if len(number_split) == 2:
+                if len(number_split[1]) == 1 and len(number_split[1].split("/")) == 1:
+                    number = number_split[0] + ".0" + number_split[1]
+                elif len(number_split[1].split("/")) == 2:
+                    if len(number_split[1].split("/")[0]) == 1:
+                        number = number_split[0] + ".0" + number_split[1]
+            else:
+                print("UNKNOWN NUMBER:", number)
+
+            self.numeris = number
+            self.has_valid_number = True
+            # Jei yra numeris, tai yra ir pavadinimas
+            self.pavadinimas = " ".join(line.split()[1:])
+        else:
+            self.numeris = "0.00"
+            self.has_valid_number = False
+            self.pavadinimas = None
+
+    def set_tipas(self):
+        dirty_tipas = (self.description.strip().split("\n")[0].replace(
+            "(Tiesioginė transliacija)", "").strip().split(" "))
+        # Remove random symbols and take only first word
+        tipas = [i for i in dirty_tipas if len(i) != 1][0]
+        self.tipas = tipas
+
+    def set_ciklas(self):
+        # Remove random symbols
+        ciklas = (self.summary
+                  .replace("(Tiesioginė transliacija)", "")
+                  .replace("🏠", " ")
+                  .replace("🔴", " ")
+                  .strip()
+                  )
+        self.ciklas = ciklas
+
+    def set_data(self):
+        start = self.dtstart.dt.strftime("%Y.%m.%d %H:%M")
+        end = self.dtend.dt.strftime("%H:%M")
+        date = start + "-" + end
+        self.data = date
+
+    def set_pavadinimas(self):
+        if not self.has_valid_number:
+            pavadinimas = self.description.split("\n")[1].strip()
+            if pavadinimas:
+                self.pavadinimas = pavadinimas
+            else:
+                pavadinimas = "None"
+        else:
+            pass
+
+
+def get_shit_list_from_file(flocation):
+    jeez_lsmu_list = []
     with open(flocation, "r", encoding="UTF-8") as fin:
         result = re.findall(
             r"([0-9]\.[0-9]+)\.\s(.*\n){,7}Padalinys –(.*)", fin.read())
@@ -22,81 +115,13 @@ def get_shit_list(flocation):
             if len(number.split(".")[1]) == 1:
                 number = ".0".join(number.split("."))
             katedra = i[2].strip()
-            fuck_lsmu = {"numeris": number, "katedra": katedra}
-            fuck_lsmu_list.append(fuck_lsmu)
-    return fuck_lsmu_list
-
-
-
-def get_cal_dates():
-
-    req = requests.get(CAL_URL)
-    if req.status_code == 200:
-        print("OK!")
-    else:
-        print("Boo!")
-        sleep(3)
-        sys.exit(req)
-    req.encoding = "UTF-8"
-    gcal = icalendar.Calendar.from_ical(req.text)
-    cal_dates = []
-
-    for component in gcal.walk():
-        if component.name == "VEVENT":
-            summ = component["DESCRIPTION"]
-            result = re.search(r"[0-9]\.[0-9]+(.*?)\n", summ)
-            if result:
-                tipas = ""
-                ciklas = ""
-                for k, v in component.items():
-                    if k == "DESCRIPTION":
-                        dirty_tipas = (
-                            v.strip()
-                            .split("\n")[0]
-                            .replace(" (Tiesioginė transliacija)", "")
-                            .strip()
-                            .split(" ")
-                        )
-                        tipas = [i for i in dirty_tipas if len(i) != 1][0]
-                    if k == "SUMMARY":
-                        ciklas = (
-                            v.replace(" (Tiesioginė transliacija)", "")
-                            .replace("🏠", " ")
-                            .replace("🔴", " ")
-                            .strip()
-                        )
-                if ciklas == CIKLAS:
-                    cname = result[0].strip()
-                    number = cname.split(" ")[0]
-                    pavadinimas = " ".join(cname.split(" ")[1:])
-
-                    if len(number.split(".")[1]) == 1:
-                        number = ".0".join(number.split("."))
-
-                    dtstart = component["DTSTART"].dt.strftime(
-                        "%Y.%m.%d %H:%M")
-                    dtend = component["DTEND"].dt.strftime("%H:%M")
-                    date = dtstart + "-" + dtend
-
-                    combo = {
-                        "numeris": number,
-                        "data": date,
-                        "pavadinimas": pavadinimas,
-                        "tipas": tipas,
-                        "ciklas": ciklas,
-                    }
-                    # print(number)
-                    # print(tipas)
-                    # print(pavadinimas)
-                    # print()
-                    cal_dates.append(combo)
-    return cal_dates
+            jeez_lsmu = {"numeris": number, "katedra": katedra}
+            jeez_lsmu_list.append(jeez_lsmu)
+    return jeez_lsmu_list
 
 
 def cal_to_csv(fname, cal_list):
     with open(fname, "w", newline="", encoding="utf-8") as f:
-        print(":::")
-        print("Opened file")
         thewriter = csv.writer(f)
         thewriter.writerow(
             ["Numeris", "Data", "Pavadinimas", "Tipas", "Ciklas"])
@@ -109,10 +134,117 @@ def cal_to_csv(fname, cal_list):
                 row["ciklas"],
             ]
             thewriter.writerow(lsmu_list)
+        print("File Saved to:", fname)
         print("All good, Bye :)")
-        print(":::")
 
 
-# Fun part
-cal_list = get_cal_dates()
-cal_to_csv(CSV_FNAME, cal_list)
+def get_cal_from_url(cal_url):
+    req = requests.get(cal_url)
+    if req.status_code == 200:
+        print("Calendar URL status: OK!")
+    else:
+        print("Calendar URL status: Boo!")
+        sleep(3)
+        sys.exit(req)
+    req.encoding = "UTF-8"
+    cal = icalendar.Calendar.from_ical(req.text)
+
+    with open("cal.ics", "w", encoding="UTF-8") as fout:
+        fout.write(req.text)
+
+    return cal
+
+
+def get_cal_from_file(cal_fname):
+    with open(cal_fname, "r", encoding="UTF-8") as fin:
+        cal = icalendar.Calendar.from_ical(fin.read())
+    return cal
+
+
+def get_event_obejct_list(calendar):
+    obj_list = []
+    for component in calendar.walk("VEVENT"):
+        description = component.get("DESCRIPTION")
+        summary = component.get("SUMMARY")
+        dtstart = component.get("DTSTART")
+        dtend = component.get("DTEND")
+        obj_list.append(Event(description, summary, dtstart, dtend))
+    return obj_list
+
+
+def get_cal_events_list(obj_list, ciklas):
+
+    events_list = []
+
+    for event in obj_list:
+        combo_dict = event.get_combo_dict(ciklas)
+        if combo_dict:
+            events_list.append(combo_dict)
+
+    return events_list
+
+
+def lol(event_object_list):
+    good_count = 0
+    bad_count = 0
+
+    for i in event_object_list:
+        if i.has_valid_number:
+            good_count += 1
+        else:
+            bad_count += 1
+
+    total_count = good_count + bad_count
+
+    print("Total Events:", total_count)
+    print("Events without 'paskaitos numeris':", bad_count)
+    print("Events with 'paskaitos numeris':", good_count)
+    print("Tinginiai, kurie nesugeba įvesti paskaitos numerio:",
+          (bad_count/total_count)*100, "%")
+
+
+def get_ciklai_list(event_object_list):
+    ciklai = []
+    for event in event_object_list:
+        if event.ciklas not in ciklai:
+            ciklai.append(event.ciklas)
+    return ciklai
+
+
+def main():
+
+    cal_url = input('Enter Calendar URL (Enter for default): ')
+    if not cal_url:
+        cal_url = "https://lsmusis.lsmuni.lt/Tvarkarasciai/ManoTvarkarastis/ICal?lang=Lt&id=LkRwDqvySWE%3D"
+
+
+    # Get Calendar from File or from URL:
+    cal = get_cal_from_url(cal_url)
+    # cal = get_cal_from_file(CAL_FNAME)
+
+    # Get Event object list from Calendar:
+    event_object_list = get_event_obejct_list(cal)
+
+    # Extras:
+    # lol(event_object_list)
+
+    options = get_ciklai_list(event_object_list)
+    terminal_menu = TerminalMenu(options, title="Pasirinkite Ciklą:")
+    menu_entry_index = terminal_menu.show()
+    print(f"Jūs pasirinkote: {options[menu_entry_index]}!")
+
+    csv_fname = input("Enter CSV file output location (enter for default): ")
+
+    if not csv_fname:
+        csv_fname = "tvarkarastis.csv"
+    
+    # Write Calendar List to CSV file:
+    cal_list = get_cal_events_list(
+        event_object_list, options[menu_entry_index])
+    cal_to_csv(csv_fname, cal_list)
+
+
+
+
+if __name__ == "__main__":
+    main()
